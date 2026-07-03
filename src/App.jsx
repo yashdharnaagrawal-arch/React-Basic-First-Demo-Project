5f0cb185e4e7f722d4e73b493f9af416a301e3f1
Yash Dharna <yashdharnaagrawal@gmail.com>
2026-07-03 20:41:01 +0530

    Footer component and App fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import FeaturedBooks from "./components/FeaturedBooks";
+import Footer from "./components/Footer";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Hero />
       <FeaturedBooks />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import "./Footer.css";
+
+function Footer(){
+
+    return(
+
+        <footer className="footer">
+
+            <p>
+                © 2026 City Library | All Rights Reserved
+            </p>
+
+        </footer>
+
+    );
+
+}
+
+export default Footer;
